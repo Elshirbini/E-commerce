be620fa1a5d7a198cb05f357519d2b9b08c76beb
Elshirbini <ahmedalshirbini33@gmail.com>
2024-11-22 13:29:09 +0200

    modified:   server/controllers/coupon.js 	modified:   server/package-lock.json 	modified:   server/package.json

--- server/controllers/coupon.js
+++ server/controllers/coupon.js
@@ -1,9 +1,36 @@
-import { ApiError } from "../utils/apiError.js"
+import { Coupon } from "../models/coupon.js";
+import { ApiError } from "../utils/apiError.js";
+import couponCode from "coupon-code";
+import { hash } from "bcrypt";
 
-export const createCoupon = (req,res,next) => {
-    try {
-        const {code , discount , expires} = req.body
-    } catch (error) {
-        next(new ApiError(error , 500))
+export const createCoupon = async (req, res, next) => {
+  try {
+    const { discount, expires } = req.body;
+
+    const date = Date.now();
+
+    if (discount > 100 || new Date(expires).getTime() <= date) {
+      return next(
+        new ApiError("discount and expire date should be valid", 400)
+      );
     }
-}
+    const code = couponCode.generate({ parts: 2, partLen: 4 });
+    const hashedCode = await hash(code, 12);
+
+    const coupon = await Coupon.create({
+      code: hashedCode,
+      discount,
+      expires,
+    });
+
+    res.status(200).json({
+      coupon: {
+        code,
+        discount,
+        expires,
+      },
+    });
+  } catch (error) {
+    next(new ApiError(error, 500));
+  }
+};
